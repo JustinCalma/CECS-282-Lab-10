98a9b97c072bc4d538017566de0b734b69efd927
Justin Calma <calma.justin.00@gmail.com>
2020-11-07 20:13:21 -0800

    Implementation file for the class PointerDataClass

--- Problem 2/PointerDataClass.cpp
+++ Problem 2/PointerDataClass.cpp
@@ -8,27 +8,50 @@ using namespace std;
 // Single argument constructor to create an array of the size specified by the parameter size 
 pointerDataClass::pointerDataClass(int size) {
 	
+	// Set the maxSize of the array to be the passed in value
+	maxSize = size;
 	
+	// Initialize the array pointer to the created array with the desired size
+	p = new int [maxSize];
+	
+//	cout << maxSize << endl;
 	
 }
 
 // Destructor to deallocate the memory space occupied by the array p
 pointerDataClass::~pointerDataClass() {
 	
-	
+	// Use the delete[] keyword to delete the array and deallocate the memory
+	delete[] p;
 	
 }
 
 // Function that inserts num into array p at the position specified by index
-pointerDataClass::insertAt(int index, int num) {
+void pointerDataClass::insertAt(int index, int num) {
 	
+	cout << "INDEX: " << index;
+	cout << " NUM: " << num << endl;
 	
-	
+	// Insert the num into array p at the desired index 
+	p[index] = num;
+
 }
 
-// Displays all the array p and all of its elements
-pointerDataClass::displayData() {
+// Displays array p and all of its elements
+void pointerDataClass::displayData() {
 	
+	// Get the total number of elements within the array p
+//	length = sizeof(p)/sizeof(p[0]);
+//	
+//	cout << length << endl;
 	
+	// Iterate through the array and print out its elements
+	for (int i = 0; i < maxSize; i++) {
+		
+		cout << *(p + i) << " ";
+		
+	}
+
+	cout << endl;
 	
 }
